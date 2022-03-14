f12812b05f699de721571ce505e3c854079ee169
SERAP KANTAROĞLU <serapkantaroglu.59@hotmail.com>
2022-03-14 15:36:48 +0300

    localStorageGetItem

--- ContextApi/intro/src/context/ThemeContext.js
+++ ContextApi/intro/src/context/ThemeContext.js
@@ -1,9 +1,14 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
 const ThemeContext = createContext(); // context oluşturma
 
 export const ThemeProvider = ({ children }) => {
-  const [theme, setTheme] = useState("Red");
+  const [theme, setTheme] = useState(localStorage.getItem("theme") || "Red");
+
+  //useEffect
+  useEffect(() => {
+    localStorage.setItem("theme", theme);
+  }, [theme]);
 
   const values = {
     theme,
@@ -16,3 +21,6 @@ export const ThemeProvider = ({ children }) => {
 };
 
 export default ThemeContext;
+
+//localStorage.getItem("theme")||"Red"  //GETİR YOKSA RED
+//localStorage.setItem("theme", theme); // KEY:THEME VALUE:THEME
